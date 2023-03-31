64ba4788a683217263ab9eaae64b230cd046c01c
arashisec <128013897+arashisec@users.noreply.github.com>
2023-03-31 18:48:41 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt update
+apt upgrade
+apt install python-gmpy2
+apt install sagemath
+pip3 install utils
+pip3 install ecdsa
+pip3 install bitcoin
+pip3 install base58
+pip3 install hashlib
+pip2 install utils
+pip2 install ecdsa
+pip2 install bitcoin
+pip2 install base58
+pip2 install hashlib
